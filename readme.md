b557d3c13362cb12a7dcef5ff6572756dfe23bc2
Summer <summer@Summers-MacBook-Pro.local>
2018-09-04 20:53:29 +0800

    Elasticsearch

--- readme.md
+++ readme.md
@@ -2,13 +2,18 @@
 
 在 `lc/homestead` 的基础上，增加了：
 
-- 默认安装 heroku
-- 国内 composer 加速
+- 默认安装 heroku 命令
+- 国内 Composer 加速
 - cnpm 国内 npm 加速
+- 默认安装 Elasticsearch
+
+## 原理
+
+基于 [laravel/homestead](https://app.vagrantup.com/laravel/boxes/homestead) 构建自定义的 BOX。
 
 ## 教程
 
 1. 下载并导入最新的 box，导入时注意修改 provider 为 `lc/homestead`  https://laravel-china.org/topics/2090 ；
 2. 检查 `vagrant box list` 是否有 `lc/homestead`;
-3. 检查 `provision.sh` 确定无误；
+3. 如果有需要的话，修改 `provision.sh` 并确定无误，这个脚本将会在新的 Box 里运行，请利用此脚本来定制软件和配置；
 4. 运行 `./build.sh` 生成定制的 box。
--- scripts/provision.sh
+++ scripts/provision.sh
@@ -1,32 +1,65 @@
 #!/usr/bin/env bash
 
-# Install heroku
+# 安装 heroku
 sudo add-apt-repository "deb https://cli-assets.heroku.com/branches/stable/apt ./"
 curl -L https://cli-assets.heroku.com/apt/release.key | sudo apt-key add -
 sudo apt-get update
 sudo apt-get install heroku
 
-# Composer speed up
+# Composer 加速
 sudo su vagrant <<'EOF'
 /usr/local/bin/composer config -g repo.packagist composer https://packagist.laravel-china.org
 EOF
 
-# Caching up Laravel with composer
-sudo su vagrant <<'EOF'
-cd ~
-/usr/local/bin/composer create-project laravel/laravel cached --prefer-dist "5.5.*"
-composer create-project laravel/laravel Laravel dev-develop
-EOF
-
-# cnpm for npm speed up
+# NPM 加速
 printf '\n#alias for cnpm\nalias cnpm="npm --registry=https://registry.npm.taobao.org \
   --cache=$HOME/.npm/.cache/cnpm \
   --disturl=https://npm.taobao.org/dist \
   --userconfig=$HOME/.cnpmrc"' >> /home/vagrant/.bashrc
 
-# Caching up npm/yarn install
+# 自动配置好所有项目
 sudo su vagrant <<'EOF'
-yarn config set registry 'https://registry.npm.taobao.org'
-cd ~/cached
-yarn install --no-bin-links
+mkdir -p ~/LaravelChina
+cd ~/LaravelChina
+git clone https://github.com/summerblue/laravel-tutorial.git l01
+cd l01
+cp .env.example .env
+composer install
+SASS_BINARY_SITE=http://npm.taobao.org/mirrors/node-sass yarn install --no-bin-links
+cd ~/LaravelChina
+git clone https://github.com/summerblue/larabbs.git l02
+cd l02
+cp .env.example .env
+composer install
+SASS_BINARY_SITE=http://npm.taobao.org/mirrors/node-sass yarn install --no-bin-links
+cd ~/LaravelChina
+git clone https://github.com/summerblue/larabbs.git l03
+cd l03
+git checkout api
+cp .env.example .env
+composer install
+SASS_BINARY_SITE=http://npm.taobao.org/mirrors/node-sass yarn install --no-bin-links
+cd ~/LaravelChina
+git clone https://github.com/summerblue/larabbs.git l04
+cd l04
+git checkout weapp
+cp .env.example .env
+composer install
+SASS_BINARY_SITE=http://npm.taobao.org/mirrors/node-sass yarn install --no-bin-links
+cd ~/LaravelChina
+git clone https://github.com/summerblue/laravel-shop.git l05
+cd l05
+cp .env.example .env
+composer install
+SASS_BINARY_SITE=http://npm.taobao.org/mirrors/node-sass yarn install --no-bin-links
 EOF
+
+# 安装 Elasticsearch
+sudo su vagrant <<'EOF'
+cd ~
+git clone https://github.com/summerblue/laravel-ubuntu-init.git
+cd laravel-ubuntu-init/16.04/
+sudo ./install_elasticsearch.sh
+systemctl disable elasticsearch.service
+EOF
+
